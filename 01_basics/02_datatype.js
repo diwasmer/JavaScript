f10802b7c6bf7779cb9ea9b45b213cf9a6ec2955
Diwas Mer <diwasmer4@gmail.com>
2026-03-21 10:18:35 +0000

    data_type

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; //uses newer version of js
+
+//alert(3+3) //this is nodejs not browser
+
+//always remember to write code in a readable way
+
+let name = "Diwas"
+let age = 20
+let login = false
+
+/*
+number => 2 to the power 53
+bigint
+string => " "
+boolean => false/true
+null => standalone value
+undefined
+symbol => unique
+
+object
+*/
+
+console.log(typeof null); //type =>object
+console.log(typeof undefined); //type =>undefined
